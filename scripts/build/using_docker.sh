726f2358ce0eca8287e69e9aeb2a020995561999
Paul Jimenez <pj@place.org>
2018-11-15 10:49:55 -0500

    rename tool-build.sh to dtools.sh

--- scripts/build/using_docker.sh
+++ scripts/build/using_docker.sh
@@ -6,15 +6,17 @@ if [ `pwd` != ${GITREPO} ]; then
 fi
 git submodule update --init --recursive
 if ! ls opentxs-proto*.deb >/dev/null ; then
-    ./scripts/build/tool-build.sh "mkdir -p deps/opentxs-proto/build && cd deps/opentxs-proto/build && cmake -DBUILD_VERBOSE=OFF -DPYTHON3:BOOL=ON .. && make"
-    ./scripts/build/tool-build.sh "cd deps/opentxs-proto/build && checkinstall -D --fstrans=yes --install=no --pkgname opentxs-proto --nodoc -y make install"
+    mkdir -p deps/opentxs-proto/build
+    ./scripts/build/dtools.sh "cd deps/opentxs-proto/build && cmake -DBUILD_VERBOSE=OFF -DPYTHON3:BOOL=ON .. && make"
+    ./scripts/build/dtools.sh "cd deps/opentxs-proto/build && checkinstall -D --fstrans=yes --install=no --pkgname opentxs-proto --nodoc -y make install"
     cp deps/opentxs-proto/build/*.deb .
 else
     echo "found `ls opentxs-proto*.deb`"
 fi
 if ! ls libopentx*.deb >/dev/null ; then
-    ./scripts/build/tool-build.sh "dpkg --install deps/opentxs-proto/build/opentxs-proto*.deb && mkdir -p build && cd build && cmake .. && make test"
-    ./scripts/build/tool-build.sh "dpkg --install deps/opentxs-proto/build/opentxs-proto*.deb && cd build && checkinstall -D --fstrans=yes --install=no --pkgname libopentxs --nodoc -y make install"
+    mkdir -p build
+    ./scripts/build/dtools.sh "dpkg --install deps/opentxs-proto/build/opentxs-proto*.deb && cd build && cmake .. && make test"
+    ./scripts/build/dtools.sh "dpkg --install deps/opentxs-proto/build/opentxs-proto*.deb && cd build && checkinstall -D --fstrans=yes --install=no --pkgname libopentxs --nodoc -y make install"
     cp build/*.deb .
 else
     echo "found `ls libopentx*.deb`"
